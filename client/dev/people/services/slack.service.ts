257dd5fd8e47f2e38934aac5dba7842820892ab0
David van de Vondervoort <dd1528@L3118-Mac.local>
2016-06-08 10:19:19 +1200

    Created a authentication services for oAuth implementation

--- client/dev/people/services/slack.service.ts
+++ client/dev/people/services/slack.service.ts
@@ -7,13 +7,13 @@ import 'rxjs/add/operator/map';
 export class SlackService {
   private url:string = 'https://slack.com/api/';
   private clientId:string = '2194929392.48648557733';
-  private clientSecret:string = 'CLIENT_SECRET';
+  private clientSecret:string = '4390442a33a0cfad285f51f3cb6911b3';
 
   constructor(private http:Http) {}
 
   authorise(code:string) {
     return this.http
-      .get(this.url + 'oauth.access&client_id='+ this.clientId + 'client_secret='+ this.clientSecret +'&code='+code)
+      .get(this.url + 'oauth.access?client_id='+ this.clientId + '&client_secret='+ this.clientSecret +'&code='+code)
       .map(resp => resp.json())
       .toPromise();
   }

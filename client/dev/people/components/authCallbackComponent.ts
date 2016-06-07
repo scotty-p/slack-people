7bb96d0c560f49f7f543c9026bce8b0540b738fe
David van de Vondervoort <dd1528@L3118-Mac.local>
2016-06-08 11:55:55 +1200

    Simplify by just using the routerOnActivate

--- client/dev/people/components/authCallbackComponent.ts
+++ client/dev/people/components/authCallbackComponent.ts
@@ -10,26 +10,20 @@ import {AuthService} from "../services/auth.service";
   directives: [MD_BUTTON_DIRECTIVES],
   providers: [SlackService, AuthService]
 })
-export class AuthCallbackComponent implements OnActivate, OnInit {
-  private code:string;
-
-  ngOnInit() {
-    this.slackService.authorise(this.code)
-      .then((resp) => {
-        console.log('authorise', resp);
-
-        // store in localStorage
-        this.authService.setAccessToken(resp.access_token);
-        this.router.navigate(['/person'])
-      })
-      .catch((err) => console.log(err.json()));
-  }
+export class AuthCallbackComponent implements OnActivate {
 
   routerOnActivate(curr:RouteSegment, prev?:RouteSegment, currTree?:RouteTree, prevTree?:RouteTree):void {
-
     let params: any = curr.parameters;
     if(params.code) {
-      this.code = params.code;
+      console.log('routerOnActivate', params.code)
+      this.slackService.authorise(this.code)
+        .then((resp) => {
+          console.log('authorise', resp);
+
+          // store in localStorage
+          this.authService.setAccessToken(resp.access_token);
+        })
+        .catch((err) => console.log(err.json()));
     }
   }
 

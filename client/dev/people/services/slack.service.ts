ee0f38bf39aef8303e05beb758ff344affc615fc
Carson Bruce <carson.bruce@solnet.co.nz>
2016-06-08 13:59:51 +1200

    added real names to list

--- client/dev/people/services/slack.service.ts
+++ client/dev/people/services/slack.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core'
 import {Http} from '@angular/http'
-import 'rxjs/add/observable/error';
 import 'rxjs/add/observable/empty';
 import 'rxjs/add/observable/from';
 import 'rxjs/add/observable/fromPromise';

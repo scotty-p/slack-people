bc5162276a5b42059f1b54ba6b54bc0f85ba32b9
David van de Vondervoort <dd1528@L3118-Mac.local>
2016-06-08 17:00:45 +1200

    Restyled Auth layout

--- client/dev/people/components/auth.cmp.ts
+++ client/dev/people/components/auth.cmp.ts
@@ -7,6 +7,7 @@ import {MD_BUTTON_DIRECTIVES} from "@angular2-material/button/button";
 @Component({
   selector: 'auth-cmp',
   templateUrl: './people/templates/auth.cmp.html',
+  styleUrls: ['./people/styles/auth.cmp.css'],
   directives: [ROUTER_DIRECTIVES, MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES],
 })
 @Routes([

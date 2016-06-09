010b6a22bdb2dd42492da84ad4886c26eece9ebb
Carson Bruce <carson.bruce@solnet.co.nz>
2016-06-09 15:54:02 +1200

    corrected dist build

--- client/dev/people/components/auth.cmp.ts
+++ client/dev/people/components/auth.cmp.ts
@@ -1,14 +1,14 @@
 import {Component} from '@angular/core';
 import {ROUTER_DIRECTIVES, Routes} from '@angular/router';
 import {AuthCallbackComponent} from "./authCallbackComponent";
-import {MD_CARD_DIRECTIVES} from "@angular2-material/card/card";
-import {MD_BUTTON_DIRECTIVES} from "@angular2-material/button/button";
+
+// import {MD_BUTTON_DIRECTIVES} from "@angular2-material/button/button";
 
 @Component({
   selector: 'auth-cmp',
   templateUrl: './people/templates/auth.cmp.html',
   styleUrls: ['./people/styles/auth.cmp.css'],
-  directives: [ROUTER_DIRECTIVES, MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES],
 })
 @Routes([
   {path: '/callback/:code', component: AuthCallbackComponent}

08550c515ac9073aa5976311e374b735a7160973
David van de Vondervoort <dd1528@L3118-Mac.local>
2016-06-10 16:03:08 +1200

    Seperate slack app vars in constants file

--- server/auth/routes/index.js
+++ server/auth/routes/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const https = require('https');
+const app = require('../../config/app.json');
 
 module.exports = class AuthRoutes {
   static init(router) {
@@ -17,9 +18,7 @@ module.exports = class AuthRoutes {
     router
       .route('/auth/authenticate')
       .get((req, res) => {
-        let clientId = '2194929392.48648557733';
-        let clientSecret = '4390442a33a0cfad285f51f3cb6911b3';
-        let url = 'https://slack.com/api/oauth.access?client_id='+ clientId + '&client_secret='+ clientSecret +'&code='+req.query.code + '&redirect_uri='+req.query.redirect_uri;
+        let url = 'https://slack.com/api/oauth.access?client_id='+ app.clientId + '&client_secret='+ app.clientSecret +'&code='+req.query.code + '&redirect_uri='+req.query.redirect_uri;
 
         https.get(url, (resp) => {
           var body = '';

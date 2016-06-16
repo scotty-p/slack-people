afc700054cc632d94dce4c515dd37aedfd215e8c
David van de Vondervoort <dd1528@L3118-Mac.local>
2016-06-16 14:19:32 +1200

    Extra directives for button and updated support cmp

--- client/dev/people/components/support.cmp.ts
+++ client/dev/people/components/support.cmp.ts
@@ -17,23 +17,23 @@ import {SolnetButton} from "./solnet/solnet-button.cmp";
       </solnet-container>
     </header>
     <section class="section-login">
-      <solnet-container>
+      <solnet-container class="section-login row">
         <span>Ready to go?</span>
-        <solnet-button >Login into Slack</solnet-button>
+        <solnet-button [routerLink]="['/login']">Login into Slack</solnet-button>
       </solnet-container>
     </section>
-    <section>
+    <section class="section-help-center">
         <h1>Help center</h1>
         <div class="container">
           <div class="panel">
             <h2>Want to add to Slack?</h2>
             <p>Sure! We&quot;d love you to use our tool, it is fun, easy to use and available to everyone.</p>
-            <solnet-button>Add to Slack</solnet-button>
+            <solnet-button primary>Add to Slack</solnet-button>
           </div>
           <div class="panel">
             <h2>Needs more information</h2>
             <p>Oh, you want to know some more about our nifty creation, sure flick an email!</p>
-            <solnet-button>slack@solnet.co.nz</solnet-button>
+            <solnet-button primary>slack@solnet.co.nz</solnet-button>
           </div>
         </div>
     </section>
@@ -65,14 +65,19 @@ import {SolnetButton} from "./solnet/solnet-button.cmp";
     .header a { color: white; text-decoration: none; border-bottom: 1px solid #49A5FF; }
     .header h2 { color: #fff; text-align:center }
     .container { display: flex; justify-content: center; flex-direction: column; }
+    .container.row { flex-direction: row;}
     .logo { width: 200px; margin: 0 auto 20px auto; }
     .game { width: 80px; height: 120px; }
     .panel { padding: 10px; }
-    .section-login { color: #fff !important; height: 150px; background-color: #1e87d7; }
-    .section-portfolio { background-color: #384652; min-height: 500px; padding: 10px; }
+    .section-login { height: 150px; background-color: #1e87d7; }
+    .section-login span { color: #fff;}
+    .section-portfolio { background-color: #384652; min-height: 500px; padding: 50px 10px; }
     .section-portfolio h3, .section-portfolio p { color: #fff; }
     .section-portfolio img, .section-portfolio .block-img { width: 300px; }
     .section-portfolio .container:nth-child(odd) { border-bottom: 1px solid #58656f}
+    .row { flex-direction: row; justify-content: center;}
+    .primary { background: #1e87d7; color: #fff; }
+    .section-help-center { padding: 30px 0; }
     
     @media screen and (min-width: 680px) {
       .container { flex-direction: row; align-items: center;}

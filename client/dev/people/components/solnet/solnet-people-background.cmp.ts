c7ae0d0efb3e002248650f259dee3370d73e9530
Carson Bruce <carson.bruce@solnet.co.nz>
2016-06-16 14:34:59 +1200

    added people background component resizing images based on window width

--- client/dev/people/components/solnet/solnet-people-background.cmp.ts
+++ client/dev/people/components/solnet/solnet-people-background.cmp.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {SlackService} from "../../services/slack.service";
+
+let windowBinding;
 
 @Component({
   selector: 'solnet-people-background',
@@ -27,7 +29,7 @@ import {SlackService} from "../../services/slack.service";
 
     .users-container img {
       opacity: 0.05;
-      flex-grow: 1;
+      flex-grow: 1;      
     }
 
     .padding {
@@ -41,16 +43,20 @@ import {SlackService} from "../../services/slack.service";
 
     <div class="users-container">
       <div class="padding"></div>
-      <img *ngFor="let user of users" *ngIf="user && user.profile && user.profile.image_192" src="{{user.profile.image_192}}"/>
+      <img [style.width]="width" [style.height]="height" *ngFor="let user of users" *ngIf="user && user.profile && user.profile.image_192" src="{{user.profile.image_192}}"/>
     </div>
 
   `,
   providers: [SlackService],
   directives: []
 })
-export class SolnetPeopleBackgroundComponent {
+export class SolnetPeopleBackgroundComponent implements OnDestroy {
 
   users: any[];
+  window: any;
+
+  width: string = '128px';
+  height: string = '128px';
 
   constructor(private slackService:SlackService) {
     slackService.getUsersAsStream()
@@ -58,6 +64,27 @@ export class SolnetPeopleBackgroundComponent {
         this.users = users;
       });
 
+    this.window = window;
+    windowBinding = this.onWindowResize.bind(this);
+    this.window.addEventListener('resize', windowBinding)
+
+  }
+
+  onWindowResize($event){
+    let width = this.getWindowWidth();
+    let tiles = Math.floor(width / 128);
+    let tileWidth = width / tiles;
+    this.width = this.height = `${tileWidth}px`;
+  }
+
+  getWindowWidth(){
+    return window.innerWidth
+      || document.documentElement.clientWidth
+      || document.body.clientWidth;
+  }
+
+  ngOnDestroy():any{
+    this.window.removeEventListener('resize', windowBinding);
   }
 
 }
